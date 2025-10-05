aaffe556176be6e63747cd89b5d0158ec72bff45
Dilrabo Usubjonova <dilrabo-vic24>
2025-10-05 09:30:18 +0500

    add vercel.json

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,23 @@
-
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .api import routers 
+from .api import routers
+
 app = FastAPI(title="A.R.I.E.S. Backend API")
 
 origins = [
     "http://localhost",
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
+client_url = os.getenv("FRONTEND_URL")
+if client_url:
+    origins.append(client_url)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
